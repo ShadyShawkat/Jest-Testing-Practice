af54734941f6f523e3ffc6a5e3ad68c0bf449586
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-08 14:50:22 +0200

    Add third task and its tests

--- tasks.js
+++ tasks.js
@@ -1,6 +1,6 @@
 const stringLength = (string) => {
   if(string.length < 1 || string.length > 10) {
-    throw new Error("string length can't be smaller than 1 character or longer than 10 characters");
+    throw new Error("string length can't be smaller than 1 character or longer than 10 characters.");
   }
   return string.length;
 };
@@ -10,4 +10,20 @@ const reverseString = (string) => {
   return string.split('').reverse().join('');
 };
 
-module.exports = {stringLength, reverseString,}
+class Calculator {
+  static add = (a,b) => a+b;
+
+  static substract = (a,b) => a-b;
+
+  static multiply = (a, b) => a*b;
+
+  static divide =  (a, b) => {
+    if(b === 0) {
+      throw new Error("Invalid operation!! Can not divide by zero.");
+    } else {
+      return a/b;
+    }
+  }
+}
+
+module.exports = {stringLength, reverseString, Calculator}
--- tasks.test.js
+++ tasks.test.js
@@ -1,4 +1,4 @@
-const {stringLength, reverseString} = require('./tasks.js');
+const {stringLength, reverseString, Calculator} = require('./tasks.js');
 
 describe('stringLength tests', () => {
   test('valid string length', () => {
@@ -17,10 +17,33 @@ describe('stringLength tests', () => {
 
 describe('reverseString tests', () => {
   test('reversing a string (1)', () => {
-    expect(reverseString('racecar')).toEqual('racecar');
+    expect(reverseString('racecar')).toBe('racecar');
   });
 
   test('reversing a string (2)', () => {
-    expect(reverseString('ABCDEFG')).toEqual('GFEDCBA');
+    expect(reverseString('ABCDEFG')).toBe('GFEDCBA');
+  });
+});
+
+
+describe('Calculator tests', () => {
+  test('adding two numbers', () => {
+    expect(Calculator.add(3, 4)).toEqual(7);
+  });
+
+  test('substracting two numbers', () => {
+    expect(Calculator.substract(3, 4)).toEqual(-1);
+  });
+
+  test('multiplying two numbers', () => {
+    expect(Calculator.multiply(3, 4)).toEqual(12);
+  });
+
+  test('dividing two numbers', () => {
+    expect(Calculator.divide(6, 2)).toEqual(3);
+  });
+
+  test('dividing by zero', () => {
+    expect(() => Calculator.divide(6, 0)).toThrow();
   });
 });
